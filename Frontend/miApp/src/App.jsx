783be93065822530adac81dfdbb4b73c11bffecd
LuisReal <lgonzalezr2611@gmail.com>
2024-04-12 17:33:19 -0600

    posicionando componentes
    posicionando componentes con react css en el dom

--- Frontend/miApp/src/App.jsx
+++ Frontend/miApp/src/App.jsx
@@ -15,36 +15,43 @@ function App() {
   return (
     
     <>
-      <h1 style={{marginLeft:280}} >Sistema de Archivos</h1>
-      <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style={{width:280, marginTop:-57}}>
-        <ul class="nav nav-pills flex-column mb-auto">
-          <li class="nav-item">
-            <a href="#" class="nav-link active" aria-current="page">
-              <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"/></svg>
-              Pantalla1
-            </a>
-          </li>
-          <li>
-            <a href="#" class="nav-link link-dark">
-              <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
-              Pantalla2
-            </a>
-          </li>
-          <li>
-            <a href="#" class="nav-link link-dark">
-              <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"/></svg>
-              Pantalla3
-            </a>
-          </li>
-        </ul>
+      <div style={{position:"relative"}}>
+        <h1 style={{backgroundColor:"rgb(249, 50, 50)", color:"white", textAlign:"center"}} >Sistema de Archivos</h1>
+        <div class="d-flex flex-column flex-shrink-0 p-3" style={{width:280, position:"absolute", height:900, backgroundColor:"rgb(255, 255, 224)"}}>
+          <ul class="nav nav-pills flex-column mb-auto">
+            <li class="nav-item">
+              <a href="#" class="nav-link active" aria-current="page">
+                <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"/></svg>
+                Pantalla1
+              </a>
+            </li>
+            <hr/>
+            <li>
+              <a href="#" class="nav-link link-dark">
+                <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
+                Pantalla2
+              </a>
+            </li>
+            <hr/>
+            <li>
+              <a href="#" class="nav-link link-dark">
+                <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"/></svg>
+                Pantalla3
+              </a>
+            </li>
+          </ul>
 
+        </div>
+
+        <div style={{position:"relative", marginLeft:280, border:"1px solid blue", height:500}}>
+        <Router>
+          <Routes>
+            <Route path="/consola" element={<Consola/>}></Route>
+          </Routes>
+        </Router>
+        </div>
       </div>
-
-      <Router>
-        <Routes>
-          <Route path="/consola" element={<Consola/>}></Route>
-        </Routes>
-      </Router>
+      
     </>
     
   )
--- Frontend/miApp/src/Components/consola.jsx
+++ Frontend/miApp/src/Components/consola.jsx
@@ -7,9 +7,10 @@ function Consola() {
 
   return (
     <>
-        <div style={{width:280, marginLeft:100}}>
-          <h1>Consola comandos</h1>
-
+        <div style={{width:280, position:"absolute"}}>
+          <input type="text" style={{height:450, width:1580, position:"absolute"}} disabled />
+          <input type="text" style={{height:50, width:1500, position:"absolute", marginTop:450}} placeholder="Ingrese comando" />
+          <button type="submit" className='btn btn-primary' style={{height:50, position:"absolute", marginLeft:1500, marginTop:450}}>Enviar</button>
         </div>
     </>
   )
